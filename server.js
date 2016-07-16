c2cbb2e7b11e6ee3c35f96bc250cf44735cb084c
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2016-07-16 03:42:23 +0000

    Initial commit of code written to test using Processing on rPi, and controlling the animations from a node server

---
+++ server.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var engines = require('consolidate');
+var mustache = require('mustache');
+
+var app = express();
+
+app.set('port', (process.env.PORT || 3000));
+app.set('views', __dirname + '/public');
+app.set('view engine', 'html');
+app.engine('html', engines.mustache);
+app.use(express.static(__dirname + '/public'));
+
+var scale = 0;
+
+app.get('/', function(req, res) {
+	//res.render('home.html')
+	var dataForLights = {
+		colors: [
+		  (0xFF * scale) << 16,
+		  (0xFF * scale) << 8,
+		  0xFF * scale,
+		  ((0xFF * scale) << 8) + (0xFF * scale)
+		]
+	};
+	res.send(dataForLights);
+	
+	scale += 0.01;
+	if(scale > 1){
+		scale = 0;
+	}
+});
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found!');
+    err.status = 404;
+    next(err);
+});
+
+app.listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'));
+});
