d16d3bf9ccae10482cd897be7aff1a85382784d2
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2016-08-02 04:43:37 +0000

    Actuator code - initial test program somewhat working but not reliable.

--- actuator.c
+++ actuator.c
@@ -1,22 +1,49 @@
+#include <stdio.h>
 #include <wiringPi.h>
 
+int dataPin = 25;
+
+void move(int repeat, int speed){
+	int i;
+	for(i = 0; i < repeat; i ++){
+		printf("%d / %d moving at speed %d\n", i, repeat, speed);
+		softPwmWrite(dataPin, speed);
+		delay(1000);
+	}
+	softPwmWrite(dataPin, 0);
+	delay(1000);
+}
 
 int main(void) {
-	
-	int dataPin = 7;
 	wiringPiSetup();
 	pinMode(dataPin, OUTPUT);
-
-	digitalWrite(dataPin, HIGH);
-	delay(1);
 	digitalWrite(dataPin, LOW);
-
-	delay(3000);
-
-	digitalWrite(dataPin, HIGH);
-	delay(2);
-	digitalWrite(dataPin, LOW);
-	delay(3000);
-
+	
+	softPwmCreate(dataPin, 0, 200);
+	delay(5000);
+	
+	int i;
+	for(i = 0; i < 5; i++){
+		
+		
+		move(1, 10);
+		move(1, 15);
+		move(1, 10);
+		move(1, 15);
+		move(20, 10);
+		
+		move(25, 15);
+		
+		move(1, 15);
+		move(1, 20);
+		move(1, 15);
+		move(1, 20);
+		move(1, 15);
+		move(20, 20);
+		
+		move(25, 15);
+	}
+	
+	return 0;
 }
 
