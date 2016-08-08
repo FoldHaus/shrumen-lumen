ca4f00ccb2dfa38e52eb39ac2bd869034cb199f7
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2016-08-08 02:23:44 +0000

    Started working on a digital version of linear actuator software

--- actuator.c
+++ actuator.c
@@ -14,35 +14,66 @@ void move(int repeat, int speed){
 	delay(1000);
 }
 
+void digiForward(dataPin) {
+	digitalWrite(dataPin, HIGH);
+	delay(2)
+	digitalWrite(dataPin, LOW);
+	delay(10);
+
+}
+
+void digiStop(dataPin) {
+	
+	digitalWrite(dataPin, HIGH);
+	delay(1.5)
+	digitalWrite(dataPin, LOW);
+	delay(10);
+
+}
+
+void digiBack(dataPin) {
+	digitalWrite(dataPin, HIGH);
+	delay(1)
+	digitalWrite(dataPin, LOW);
+	delay(10);
+
+}
+
+
+
 int main(void) {
 	wiringPiSetup();
 	pinMode(dataPin, OUTPUT);
-	digitalWrite(dataPin, LOW);
+	//~ digitalWrite(dataPin, LOW);
 	
-	softPwmCreate(dataPin, 0, 200);
-	delay(5000);
-	
-	int i;
 	for(i = 0; i < 5; i++){
 		
-		
-		move(1, 10);
-		move(1, 15);
-		move(1, 10);
-		move(1, 15);
-		move(20, 10);
-		
-		move(25, 15);
-		
-		move(1, 15);
-		move(1, 20);
-		move(1, 15);
-		move(1, 20);
-		move(1, 15);
-		move(20, 20);
-		
-		move(25, 15);
 	}
+	
+	//~ softPwmCreate(dataPin, 0, 200);
+	//~ delay(5000);
+	
+	//~ int i;
+	//~ for(i = 0; i < 5; i++){
+		//~ 
+		//~ 
+		//~ move(1, 10);
+		//~ move(1, 15);
+		//~ move(1, 10);
+		//~ move(1, 15);
+		//~ move(20, 10);
+		//~ 
+		//~ move(25, 15);
+		//~ 
+		//~ move(1, 15);
+		//~ move(1, 20);
+		//~ move(1, 15);
+		//~ move(1, 20);
+		//~ move(1, 15);
+		//~ move(20, 20);
+		//~ 
+		//~ move(25, 15);
+	//~ }
 	
 	return 0;
 }
