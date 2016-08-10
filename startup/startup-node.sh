8cbb957f1de240e8890efbac89da07ae680dadd6
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2016-08-10 01:28:58 +0000

    Corrected the startup-node.sh file to only start the node server

--- startup/startup-node.sh
+++ startup/startup-node.sh
@@ -2,6 +2,5 @@
 
 echo "Starting Node server..."
 echo `which node`
-sudo /usr/bin/node /home/pi/Desktop/pi-server/server.js &
-sudo bash /home/pi/Desktop/pi-server/WeightSensor/sensor2.sh
+sudo /usr/bin/node /home/pi/Desktop/pi-server/server.js
 
