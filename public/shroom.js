0e6ed14e195a42605eed3c7f49d28566142d6d73
David Brody <dbrody@ideo.com>
2016-08-16 21:31:08 -0700

    Updates web UI to show weight sensor data.

--- public/shroom.js
+++ public/shroom.js
@@ -5,10 +5,27 @@ class ShroomAPI {
     this.connected = true;
   }
 
+  updateState() {
+    var self = this;
+    self.getState();
+    self.getLinearActuator();
+    self.getWeightSensor();
+  }
+
   getState() {
     var self = this;
     this.$http.get(this.ip + "/state", {timeout: 1000}).then(function(data){
       self.data = data.data;
+      self.connected = true;
+    }, function(){
+      self.connected = self.connected && false;
+    });
+  }
+
+  getWeightSensor() {
+    var self = this;
+    this.$http.get(this.ip + "/weightsensor", {timeout: 1000}).then(function(data){
+      self.weightsensor = data.data;
       self.connected = true;
     }, function(){
       self.connected = self.connected && false;
@@ -31,6 +48,7 @@ class ShroomAPI {
 
 angular.module('todoApp', [])
   .controller('ShroomController', function($http) {
+    var self = this;
     this.ips = [
       'http://10.1.3.115:3000',
       'http://1.1.1.1:3000',
@@ -44,9 +62,13 @@ angular.module('todoApp', [])
 
     for(var ipI in this.ips){
       var ip = this.ips[ipI];
-      var shroomC = new ShroomAPI(ip, $http);
-      shroomC.getState();
-      shroomC.getLinearActuator();
-      this.shrooms.push(shroomC);
+      var shroom = new ShroomAPI(ip, $http);
+      this.shrooms.push(shroom);
     }
+
+    setInterval(function(){
+      for(var shroom in self.shrooms){
+        self.shrooms[shroom].updateState();
+      }
+    }, 500);
   });
