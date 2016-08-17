9c1cef679bc9516abd7cf3975cabd2704c087849
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2016-08-17 03:43:20 +0000

    Adds cross-origin access to server for remote API access.

--- server.js
+++ server.js
@@ -17,6 +17,12 @@ app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
+app.use(function(req, res, next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 //Add a module containing a class for interacting with the WeightSensor
 var WeightSensorInterface = require("./lib/WeightSensorInterface.js");
 var weightSensorInterface = new WeightSensorInterface();
