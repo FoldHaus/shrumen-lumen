5cfb1a3893c7c1af76690c33496c5310845225a2
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2017-10-01 08:46:23 -0700

    Updates sketch with basic control over motor controller using pressure pad and adds a signal LED

--- ShrumenLumen/ShrumenLumen.ino
+++ ShrumenLumen/ShrumenLumen.ino
@@ -1,17 +1,34 @@
 #include "MotorController.h"
 
 // Pin Definitions
-int directionDigitalPin = PIN_A8;
-int speedPWMPin = PIN_A9;
+int directionDigitalPin = 12;
+int speedPWMPin = 23;
+const int padPin = 2;
+const int ledPin =  13;
+
+// Other Variable Definitions
+int padState = 0;
 
 // Class Instantiations
 MotorController motorController(directionDigitalPin, speedPWMPin);
 
 void setup() {
-  analogWrite(speedPWMPin, 0);
+  pinMode(ledPin, OUTPUT); 
+  pinMode(padPin, INPUT);  
+  
+  analogWrite(speedPWMPin, 0); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  motorController.extend();
+  padState = digitalRead(padPin);
+
+  if (padState == HIGH) {         
+    digitalWrite(ledPin, HIGH); 
+    motorController.extend(); 
+    
+  } 
+  else {
+    digitalWrite(ledPin, LOW); 
+    motorController.retract();
+  }
 }
