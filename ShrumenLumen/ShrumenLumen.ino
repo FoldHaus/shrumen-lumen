f03dc916cd56324258ac37d2c80a790c5dbecc84
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2017-12-27 00:10:09 -0500

    Replicates initial pad animation and basic animation sequence

--- ShrumenLumen/ShrumenLumen.ino
+++ ShrumenLumen/ShrumenLumen.ino
@@ -9,26 +9,43 @@ const int PAD_PIN = 2;
 const int NEOPIXEL_DATA_PIN = 3;
 
 //Config Constants
-const bool MOTOR_CONTROLLER_DEBUG_MODE = true;
+const bool MOTOR_CONTROLLER_DEBUG_MODE = false;
 const int LED_STRIP_LENGTH = 16;
 
-// Animation Timings
+// Sequence Timings
 const unsigned long extensionTime = 15000;
 const unsigned long retractionTime = 15000;
 const unsigned long freezeTime = 30000;
 const unsigned long refreshTime = 30000;
+const unsigned long sequenceTimings[4] = {
+  extensionTime, 
+  extensionTime + retractionTime, 
+  extensionTime + retractionTime + freezeTime, 
+  extensionTime + retractionTime + freezeTime + refreshTime
+};
 
-// State Variables
+// Sequence State Variables
 int padState = LOW;
-int ledSequence = 0;
-int rainbowCount = 0;
 bool inProgress = false;
 unsigned long timer = 0;
-unsigned long sequenceProgressAcc = 0;
+
+// LED Animation State Variables 
+// (Async animations functions need state variable counters)
+unsigned long int ledSequence = 0;
+unsigned long int rainbowCount = 0;
+unsigned long int colorWipeTimer = 0;
+unsigned long int colorWipePixelNum = 0;
 
 // Class Instantiations
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_STRIP_LENGTH, NEOPIXEL_DATA_PIN);
-MotorController motorController(DIRECTION_DIGIAL_PIN, SPEED_PWM_PIN, MOTOR_CONTROLLER_DEBUG_MODE);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(
+  LED_STRIP_LENGTH, 
+  NEOPIXEL_DATA_PIN
+);
+MotorController motorController(
+  DIRECTION_DIGIAL_PIN, 
+  SPEED_PWM_PIN, 
+  MOTOR_CONTROLLER_DEBUG_MODE
+);
 
 void setup() {
   pinMode(PAD_PIN, INPUT);
@@ -46,7 +63,8 @@ void loop() {
     if (!inProgress) {
       timer = millis();
     }
-    
+
+    // Start animation sequence    
     animate();
   }
   else {
@@ -61,51 +79,34 @@ void loop() {
 void animate() {
   inProgress = true;
   unsigned long delayTime = millis() - timer;
-  Serial.println(delayTime);
 
   // Extension Phase 
-  if (delayTime <= extensionTime) {
+  if (delayTime <= sequenceTimings[0]) {
     motorController.extend();
     ledSequence = 1;
-    sequenceProgressAcc = extensionTime;
-    Serial.println("Extension Phase");
-    Serial.println("Sequence Acc");
-    Serial.println(sequenceProgressAcc);
   } 
   // Freeze Phase   
-  else if (delayTime > sequenceProgressAcc && delayTime <= freezeTime + sequenceProgressAcc) {
+  else if (delayTime > sequenceTimings[0] && delayTime <= sequenceTimings[1]) {
     motorController.freeze();
     ledSequence = 0;
-    sequenceProgressAcc = freezeTime + extensionTime;
-    Serial.println("Freeze Phase");
-    Serial.println("Sequence Acc");
-    Serial.println(sequenceProgressAcc);
   }
   // Retraction Phase   
-  else if (delayTime > sequenceProgressAcc && delayTime < retractionTime + sequenceProgressAcc) {
+  else if (delayTime > sequenceTimings[1] && delayTime <= sequenceTimings[2]) {
     motorController.retract();
     ledSequence = 2;
-    sequenceProgressAcc = retractionTime + freezeTime + extensionTime;
-    Serial.println("Retraction Phase");
-    Serial.println("Sequence Acc");
-    Serial.println(sequenceProgressAcc);
   } 
   // Refresh Phase 
-  else if (delayTime > sequenceProgressAcc && delayTime <= sequenceProgressAcc + refreshTime) {
+  else if (delayTime > sequenceTimings[2] && delayTime <= sequenceTimings[3]) {
     motorController.freeze();
     ledSequence = 0;
-    sequenceProgressAcc = refreshTime + retractionTime + freezeTime + extensionTime;
-    Serial.println("Refresh Phase");
-    Serial.println("Sequence Acc");
-    Serial.println(sequenceProgressAcc);
   } 
   // End of Sequence 
   else {
-    // Reset all the state variables
+    // Reset state variables
     inProgress = false;
-    sequenceProgressAcc = 0;
-    
-    Serial.println("Animation End");
+    rainbowCount = 0;
+    colorWipeTimer = 0;
+    colorWipePixelNum = 0;
   }
 
   lightStrip(ledSequence);
@@ -113,56 +114,39 @@ void animate() {
 
 void lightStrip(int lightSequence) {
   // 0 - Wipe to Dark 
-  // 1 - Theater Chase
-  // 2 - Rainbow
-
+  // 1 - Wipe to Green
+  // 2 - Wipe to Red
+  // 3 - Wipe to Purple
+  
   switch (lightSequence) {
     case 0:
-      colorWipe(0);
+      colorWipe(0, 0, 0);
       break;
     case 1:
-      rainbowCycle();
+      colorWipe(0, 255, 0);
       break;
     case 2:
-      colorWipe(27);
+      colorWipe(255, 15, 0);
       break;
     default:
-      rainbowCycle();
+      colorWipe(255, 0, 255);
       break;
   }
-}
-
-// Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle() {
-  uint16_t i;
-  rainbowCount += 1;
-
-  for(i=0; i< strip.numPixels(); i++) {
-    strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + rainbowCount) & 255));
-  }
-
-  strip.show();
 }
 
 // Fill the dots one after the other with a color
-void colorWipe(uint32_t c) {
-  for (uint16_t i = 0; i < strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
+void colorWipe(uint32_t r, uint32_t g, uint32_t b) {
+  if (colorWipeTimer % 2 == 0) {
+    strip.setPixelColor(
+      colorWipePixelNum % strip.numPixels(), 
+      r, 
+      g,
+      b
+    );
     strip.show();
-  }
-}
 
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if (WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    colorWipePixelNum += 1;
   }
-  if (WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+
+  colorWipeTimer += 1;
 }
